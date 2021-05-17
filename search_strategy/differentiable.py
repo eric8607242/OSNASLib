1d8ef604df51fdb6c9fe1d96f60d0069dbc2f6d3
eric8607242 <e0928021388@gmail.com>
2021-05-17 14:44:50 +0800

    differentiable config, logs

--- search_strategy/differentiable.py
+++ search_strategy/differentiable.py
@@ -1,24 +1,28 @@
-from utils import get_optimizer
-from criterion import get_hc_criterion
+import torch
+import torch.nn as nn
+
+from utils import get_optimizer, min_max_normalize, save, AverageMeter, accuracy
+from criterion import get_hc_criterion, get_criterion
 
 from .base import BaseSearcher
 
 class DifferentiableSearcher(BaseSearcher):
-    def __init__(self, supernet, val_loader, lookup_table, training_strategy, device, logger):
-        super(DifferentiableSearcher, self).__init__(supernet, val_loader, lookup_table, training_strategy, device, logger)
+    def __init__(self, config, supernet, val_loader, lookup_table, training_strategy, device, logger):
+        super(DifferentiableSearcher, self).__init__(config, supernet, val_loader, lookup_table, training_strategy, device, logger)
 
         # Init architecture parameter optimizer
         self.a_optimizer = get_optimizer(
-            [supernet.module.get_architecture_param() if isinstance(supernet, nn.DataParallel) \
-                    else supernet.get_architecture_param()],
-            config["arch_optim"]["a_optimizer"],
-            learning_rate=config["arch_optim"]["a_lr"],
-            weight_decay=config["arch_optim"]["a_weight_decay"],
-            logger=logger,
-            momentum=config["arch_optim"]["a_momentum"],
-            alpha=config["arch_optim"]["a_alpha"],
-            beta=config["arch_optim"]["a_beta"])
+            [supernet.module.get_arch_param() if isinstance(supernet, nn.DataParallel) \
+                    else supernet.get_arch_param()],
+            self.config["arch_optim"]["a_optimizer"],
+            learning_rate=self.config["arch_optim"]["a_lr"],
+            weight_decay=self.config["arch_optim"]["a_weight_decay"],
+            logger=self.logger,
+            momentum=self.config["arch_optim"]["a_momentum"],
+            alpha=self.config["arch_optim"]["a_alpha"],
+            beta=self.config["arch_optim"]["a_beta"])
 
+        self.criterion = get_criterion(self.config["train"]["criterion_type"])
         self.hc_criterion = get_hc_criterion(self.config["train"]["hc_criterion_type"])
 
         self.top1 = AverageMeter()
@@ -38,23 +42,25 @@ class DifferentiableSearcher(BaseSearcher):
         # Update architecture parameter
         self.a_optimizer.zero_grad()
 
-        architecture_parameter = self.supernet.module.get_architecture_param() if isinstance(
-            self.supernet, nn.DataParallel) else self.supernet.get_architecture_param()
+        architecture_parameter = self.supernet.module.get_arch_param() if isinstance(
+            self.supernet, nn.DataParallel) else self.supernet.get_arch_param()
         architecture_info = self.lookup_table.get_model_info(
             architecture_parameter)
 
-        hc_loss = self.hc_criterion(architecture_info, self.target_hc)*self.config["arch_optim"]["hc_weight"]
+        target_hc_tensor = torch.tensor(self.target_hc, dtype=torch.float).to(self.device)
+        hc_loss = self.hc_criterion(architecture_info, target_hc_tensor)*self.config["arch_optim"]["hc_weight"]
 
         X, y = next(iter(self.val_loader))
         X, y = X.to(
             self.device, non_blocking=True), y.to(
             self.device, non_blocking=True)
+        N = X.shape[0]
 
         outs = self.supernet(X)
         ce_loss = self.criterion(outs, y)
 
         total_loss = hc_loss + ce_loss
-        self.logger.info(f"Hardware loss : {hc_loss.item()}")
+#         self.logger.info(f"Hardware loss : {hc_loss.item()}")
 
         total_loss.backward()
         self.a_optimizer.step()
@@ -69,18 +75,18 @@ class DifferentiableSearcher(BaseSearcher):
             val_or_train="ArchParam_Train")
 
     def search(self):
-        best_architecture = self.supernet.module.get_best_architecture_param() if isinstance(
-            self.supernet, nn.DataParallel) else self.supernet.get_best_architecture_param()
+        best_architecture = self.supernet.module.get_best_arch_param() if isinstance(
+            self.supernet, nn.DataParallel) else self.supernet.get_best_arch_param()
 
         #self.supernet.module.set_activate_architecture(best_architecture) if isinstance(
             #self.supernet,
             #nn.DataParallel) else self.supernet.set_activate_architecture(best_architecture)
         #best_architecture_top1 = trainer.validate(
             #self.supernet, self.val_loader, 0)
-        return best_architecture
+        return best_architecture, _, _
     
 
-    def _intermediate_stats_logging(self, outs, y, ce_loss, hc_loss, N, len_loader, val_or_train, print_freq=100):
+    def _intermediate_stats_logging(self, outs, y, ce_loss, hc_loss, N, len_loader, val_or_train, print_freq=50):
         step = self.step_num
 
         total_loss = ce_loss + hc_loss
