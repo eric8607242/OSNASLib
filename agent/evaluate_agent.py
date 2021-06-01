627eee14d171b5d388bf63d104f9eb50daacdee6
eric8607242 <e0928021388@gmail.com>
2021-06-01 14:05:28 +0800

    evaluate agent

--- agent/evaluate_agent.py
+++ agent/evaluate_agent.py
@@ -6,7 +6,7 @@ import torch.nn as nn
 from .base_agent import MetaAgent
 
 from utils import get_optimizer, get_lr_scheduler, resume_checkpoint
-from model import Model, load_architecture
+from model import Model, load_architecture, get_supernet
 
 class EvaluateAgent(MetaAgent):
     def __init__(self, config):
@@ -15,51 +15,59 @@ class EvaluateAgent(MetaAgent):
         # Construct model and correspond optimizer ======================================
         architecture = load_architecture(self.config.searched_model_path)
 
-        self.model = Model(
+        supernet_class = get_supernet(self.config["agent"]["supernet_agent"])
+        supernet = supernet_class(
+            self.config["dataset"]["classes"],
+            self.config["dataset"]["dataset"],
+            self.config["search_utility"]["search_strategy"],
+            bn_momentum=self.config["train"]["bn_momentum"],
+            bn_track_running_stats=self.config["train"]["bn_track_running_stats"])
+        self.macro_cfg, self.micro_cfg = self.supernet.get_model_cfg()
+
+        model = Model(
             self.macro_cfg,
             self.micro_cfg,
             architecture,
-            self.config.classes,
-            self.config.dataset)
+            self.config["dataset"]["classes"]
+            self.config["dataset"]["dataset"])
+        self.model = model.to(self.device)
 
         self.optimizer = get_optimizer(
-            self.model.parameters(),
-            self.config.optimizer,
-            learning_rate=self.config.lr,
-            weight_decay=self.config.weight_decay,
+            self.supernet.parameters(),
+            self.config["optim"]["optimizer"],
+            learning_rate=self.config["optim"]["lr"],
+            weight_decay=self.config["optim"]["weight_decay"],
             logger=self.logger,
-            momentum=self.config.momentum,
-            alpha=self.config.alpha,
-            beta=self.config.beta)
+            momentum=self.config["optim"]["momentum"],
+            alpha=self.config["optim"]["alpha"],
+            beta=self.config["optim"]["beta"])
 
         self.lr_scheduler = get_lr_scheduler(
             self.optimizer,
-            self.config.lr_scheduler,
+            self.config["optim"]["scheduler"],
             self.logger,
             step_per_epoch=len(
                 self.train_loader),
-            step_size=self.config.decay_step,
-            decay_ratio=self.config.decay_ratio,
-            total_epochs=self.config.epochs)
+            step_size=self.config["optim"]["decay_step"],
+            decay_ratio=self.config["optim"]["decay_ratio"],
+            total_epochs=self.config["train"]["epochs"])
         # =================================================================================
 
         # Resume checkpoint ===============================================================
-        self.start_epoch = 0
-        if self.config.resume:
-            self.start_epoch = resume_checkpoint(
-                self.model,
-                self.config.resume,
-                self.optimizer,
-                self.lr_scheduler)
-            logger.info(
+        if self.config["train"]["resume"]:
+            self.start_epochs = resume_checkpoint(
+                    self.model,
+                    self.config["experiment_path"]["resume_path"],
+                    self.optimizer,
+                    self.lr_scheduler)
+            self.logger.info(
                 "Resume training from {} at epoch {}".format(
-                    self.config.resume, start_epoch))
+                    self.config["experiment_path"]["resume_path"], self.start_epochs))
         # =================================================================================
 
-        if device.type == "cuda" and self.config.ngpu >= 1:
-            self.model = self.model.to(self.device)
+        if self.device.type == "cuda" and self.config["train"]["ngpu"] >= 1:
             self.model = nn.DataParallel(
-                self.model, list(range(self.config.ngpu)))
+                self.model, list(range(self.config["train"]["ngpu"])))
 
     def fit(self):
         self.evaluate()
@@ -67,13 +75,14 @@ class EvaluateAgent(MetaAgent):
 
     def evaluate(self):
         start_time = time.time()
+        self.logger.info("Evaluating process start!")
+
         self.train_loop(
             self.model,
             self.train_loader,
             self.test_loader,
             self.optimizer,
-            self.lr_scheduler,
-            self.start_epoch)
+            self.lr_scheduler)
         logger.info(
             "Total search time : {:.2f}".format(
                 time.time() - start_time))
