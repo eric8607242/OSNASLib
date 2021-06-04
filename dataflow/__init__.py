1c21ab69d31b7b73a17a8edccbab7b053d076188
eric8607242 <e0928021388@gmail.com>
2021-06-04 22:11:02 +0800

    factory dataflow method, reconstruct cifar dataset

--- dataflow/__init__.py
+++ dataflow/__init__.py
@@ -1,18 +1,13 @@
 import torch
 
-from .cifar import get_CIFAR_dataloader
+from .cifar import get_cifar100_dataloader, get_cifar10_dataloader
 from .imagenet import get_imagenet_dataloader
 from .face import get_face_dataloader
 
 
 def get_dataloader(dataset_name, dataset_path, input_size, batch_size, num_workers, train_portion=1):
-    if dataset_name == "cifar10" or dataset_name == "cifar100":
-        return get_CIFAR_dataloader(dataset_name, dataset_path, input_size, batch_size, num_workers, train_portion)
-    elif dataset_name == "imagenet":
-        return get_imagenet_dataloader(dataset_name, dataset_path, input_size, batch_size, num_workers, train_portion)
-    elif dataset_name == "face":
-        return get_face_dataloader(dataset_name, dataset_path, input_size, batch_size, num_workers, train_portion)
-    else:
-        raise 
+    dataloader_builder = getattr(sys.modules[__name__], dataset_name)
+
+    return dataloader_builder(dataset_path, input_size, batch_size, num_workers, train_portion)
 
 
