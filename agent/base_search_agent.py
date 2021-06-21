input size calculation fix

agent/base_search_agent.py
@@ -34,7 +34,7 @@ class MetaSearchAgent(MetaAgent):
         self.training_strategy = training_strategy_class(self.supernet)
 
         macro_len, micro_len = self.supernet.module.get_model_cfg_shape() \
-                                    if isinstance(self.supernet, nn.DataParallel) else self.model.get_model_cfg_shape()
+                                    if isinstance(self.supernet, nn.DataParallel) else self.supernet.get_model_cfg_shape()
         self.lookup_table = lookup_table_class(
             self.macro_cfg,
             self.micro_cfg,